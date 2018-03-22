cc1c4d9060595ffcdfdbcf0a9951238a76ef28a3
Katrin Sigurdardottir <katrinduasig@gmail.com>
2018-03-22 11:53:52 +0100

    front routing

--- Angular/Angular-Cookbook/src/app/app-routing.module.ts
+++ Angular/Angular-Cookbook/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { AllRecipesComponent } from './all-recipes/all-recipes.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'all-recipres', component: AllRecipesComponent },
+  { path: 'add-recipre', component: AddRecipeComponent },
+  { path: 'log-in', component: LogInComponent},
+  // { path: 'contact', component: ContactComponent, canActivate: [AuthGuardService] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/Angular-Cookbook/src/app/app.module.ts
+++ Angular/Angular-Cookbook/src/app/app.module.ts
@@ -9,12 +9,28 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { AllRecipesComponent } from './all-recipes/all-recipes.component';
+import { AdminComponent } from './admin/admin.component';
+import { MyRecipesComponent } from './admin/my-recipes/my-recipes.component';
+import { MyProfileComponent } from './admin/my-profile/my-profile.component';
+import { MyFavoriteComponent } from './admin/my-favorite/my-favorite.component';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    AddRecipeComponent,
+    AllRecipesComponent,
+    AdminComponent,
+    MyRecipesComponent,
+    MyProfileComponent,
+    MyFavoriteComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +39,8 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule, 
     MatButtonModule, 
     MatCheckboxModule,
+    MatToolbarModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
